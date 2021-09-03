266800027042bc12e97d3fa01c232e65213b752b
MeeWeeL <MeeWeeL@yandex.ru>
2021-09-03 17:47:05 +0300

    Ethernet

--- app/src/main/java/com/meeweel/myapplication/view/DetailsFragment.kt
+++ app/src/main/java/com/meeweel/myapplication/view/DetailsFragment.kt
@@ -1,38 +1,71 @@
 package com.meeweel.myapplication.view
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import com.meeweel.myapplication.BuildConfig
+import com.meeweel.myapplication.BuildConfig.WEATHER_API_KEY
 import com.meeweel.myapplication.R
 import com.meeweel.myapplication.databinding.DetailsFragmentBinding
 import com.meeweel.myapplication.model.data.Weather
+import com.meeweel.myapplication.model.dto.WeatherDTO
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
+
+private const val YOUR_API_KEY = BuildConfig.WEATHER_API_KEY
 
 class DetailsFragment : Fragment() {
 
     private var _binding: DetailsFragmentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var weatherBundle: Weather
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = DetailsFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val weather = arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let { weather ->
-            populateData(weather)
+        weatherBundle = arguments?.getParcelable<Weather>(BUNDLE_EXTRA) ?: Weather()
+        binding.main.hide()
+        binding.loadingLayout.show()
+        val loader = WeatherLoader(onLoadListener, weatherBundle.city.lat, weatherBundle.city.lon)
+        loader.loadWeather()
+    }
+
+    private val onLoadListener = object: WeatherLoader.WeatherLoaderListener{
+        override fun onLoaded(weatherDTO: WeatherDTO) {
+            displayWeather(weatherDTO)
+        }
+        override fun onFailed(throwable: Throwable) {
+            // Обработка ошибок
         }
     }
 
-    private fun populateData(weatherData: Weather) {
+
+    private fun displayWeather(weatherDTO: WeatherDTO) {
         with(binding) {
-            weatherData.city.also{ city ->
+            main.show()
+            loadingLayout.hide()
+            weatherBundle.city.also{ city ->
                 cityName.text = city.city
                 cityCoordinates.text = String.format(
                     getString(R.string.city_coordinates),
@@ -41,9 +74,10 @@ class DetailsFragment : Fragment() {
                 )
             }
 
-            weatherData.apply {
-                temperatureValue.text = temperature.toString()
-                feelsLikeValue.text = feelsLike.toString()
+            weatherDTO.fact?.let { fact ->
+                temperatureValue.text = fact.temp.toString()
+                feelsLikeValue.text = fact.feels_like.toString()
+                weatherCondition.text = fact.condition
             }
         }
     }
